ca203af1cda29158d4193f3a0b5fb66a8b3e0470
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-09 13:48:43 +0530

    initial commit

---
+++ src/test/java/org/microq/support/MicroQueueSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microq.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroQueueSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
